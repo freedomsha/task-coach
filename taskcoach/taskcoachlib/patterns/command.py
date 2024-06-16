1b4fb51dc3beea1944cf7b16958765f3f53d9c1e
sylvain <szydlowskis@aol.com>
2024-06-16 10:43:20 +0200

    Docstrings command & composite

--- taskcoach/taskcoachlib/patterns/command.py
+++ taskcoach/taskcoachlib/patterns/command.py
@@ -20,67 +20,189 @@ from . import singleton as patterns
 
 
 class Command(object):
+    """
+    Base class for all commands.
+
+    Methods:
+        do(): Execute the command and append it to the command history.
+        undo(): Undo the command.
+        redo(): Redo the command.
+        __str__(): Return a string representation of the command.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initialize the command.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         super(Command, self).__init__()  # object.__init__ takes no arguments
 
     def do(self):
+        """
+        Execute the command and append it to the command history.
+        """
         CommandHistory().append(self)
 
     def undo(self):
+        """
+        Undo the command.
+        """
         pass
 
     def redo(self):
+        """
+        Redo the command.
+        """
         pass
 
     def __str__(self):
+        """
+        Return a string representation of the command.
+
+        Returns:
+            str: The string representation of the command.
+        """
         return "command"
 
 
 class CommandHistory(object, metaclass=patterns.Singleton):
+    """
+    Singleton class that keeps track of command history.
+
+    Attributes:
+        __history (list): The list of executed commands.
+        __future (list): The list of commands that have been undone and can be redone.
+
+    Methods:
+        append(command): Add a command to the history.
+        undo(): Undo the last command.
+        redo(): Redo the last undone command.
+        clear(): Clear the command history.
+        hasHistory(): Check if there are executed commands in the history.
+        getHistory(): Get the list of executed commands.
+        hasFuture(): Check if there are commands that can be redone.
+        getFuture(): Get the list of commands that can be redone.
+        undostr(label): Get a string label for the undo operation.
+        redostr(label): Get a string label for the redo operation.
+    """
+
     def __init__(self):
+        """
+        Initialize the command history with empty lists for history and future commands.
+        """
         self.__history = []
         self.__future = []
 
     def append(self, command):
+        """
+        Add a command to the history and clear the future commands.
+
+        Args:
+            command (Command): The command to add to the history.
+        """
         self.__history.append(command)
         del self.__future[:]
 
     def undo(self):
+        """
+        Undo the last command and add it to the future commands list.
+        """
         if self.__history:
             command = self.__history.pop()
             command.undo()
             self.__future.append(command)
 
     def redo(self):
+        """
+        Redo the last undone command and add it back to the history.
+        """
         if self.__future:
             command = self.__future.pop()
             command.redo()
             self.__history.append(command)
 
     def clear(self):
+        """
+        Clear the command history and future commands.
+        """
         del self.__history[:]
         del self.__future[:]
 
     def hasHistory(self):
+        """
+        Check if there are executed commands in the history.
+
+        Returns:
+            list: The list of executed commands.
+        """
         return self.__history
 
     def getHistory(self):
+        """
+        Get the list of executed commands.
+
+        Returns:
+            list: The list of executed commands.
+        """
         return self.__history
 
     def hasFuture(self):
+        """
+        Check if there are commands that can be redone.
+
+        Returns:
+            list: The list of commands that can be redone.
+        """
         return self.__future
 
     def getFuture(self):
+        """
+        Get the list of commands that can be redone.
+
+        Returns:
+            list: The list of commands that can be redone.
+        """
         return self.__future
 
     def _extendLabel(self, label, commandList):
+        """
+        Extend the label with the name of the last command in the command list.
+
+        Args:
+            label (str): The label to extend.
+            commandList (list): The list of commands.
+
+        Returns:
+            str: The extended label.
+        """
         if commandList:
             commandName = " %s" % commandList[-1]
             label += commandName.lower()
         return label
 
     def undostr(self, label="Undo"):
+        """
+        Get a string label for the undo operation.
+
+        Args:
+            label (str): The base label for the undo operation.
+
+        Returns:
+            str: The extended label for the undo operation.
+        """
         return self._extendLabel(label, self.__history)
 
     def redostr(self, label="Redo"):
+        """
+        Get a string label for the redo operation.
+
+        Args:
+            label (str): The base label for the redo operation.
+
+        Returns:
+            str: The extended label for the redo operation.
+        """
         return self._extendLabel(label, self.__future)
--- taskcoach/taskcoachlib/patterns/composite.py
+++ taskcoach/taskcoachlib/patterns/composite.py
@@ -21,7 +21,22 @@ import weakref
 
 
 class Composite(object):
+    """
+    A class representing a composite object in the composite pattern.
+
+    Attributes:
+        __parent (weakref): Weak reference to the parent composite.
+        __children (list): List of child composites.
+    """
+
     def __init__(self, children=None, parent=None):
+        """
+        Initialize the composite with an optional list of children and parent.
+
+        Args:
+            children (list, optional): List of child composites.
+            parent (Composite, optional): Parent composite.
+        """
         super(Composite, self).__init__()
         self.__parent = parent if parent is None else weakref.ref(parent)
         self.__children = children or []
@@ -29,16 +44,33 @@ class Composite(object):
             child.setParent(self)
 
     def __getstate__(self):
+        """
+        Get the state for pickling.
+
+        Returns:
+            dict: The state of the composite.
+        """
         return dict(children=self.__children[:], parent=self.parent())
 
     def __setstate__(self, state):
+        """
+        Set the state from unpickling.
+
+        Args:
+            state (dict): The state of the composite.
+        """
         self.__parent = (
             None if state["parent"] is None else weakref.ref(state["parent"])
         )
         self.__children = state["children"]
 
     def __getcopystate__(self):
-        """Return the information needed to create a copy as a dict."""
+        """
+        Get the state for copying.
+
+        Returns:
+            dict: The state of the composite for copying.
+        """
         try:
             state = super(Composite, self).__getcopystate__()
         except AttributeError:
@@ -52,25 +84,52 @@ class Composite(object):
         return state
 
     def parent(self):
+        """
+        Get the parent composite.
+
+        Returns:
+            Composite: The parent composite.
+        """
         return None if self.__parent is None else self.__parent()
 
     def ancestors(self):
-        """Return the parent, and its parent, etc., as a list."""
+        """
+        Get the list of ancestors of the composite.
+
+        Returns:
+            list: The list of ancestors.
+        """
         parent = self.parent()
         return parent.ancestors() + [parent] if parent else []
 
     def family(self):
-        """Return this object, its ancestors and all of its children
-        (recursively)."""
+        """
+        Get the family of the composite (ancestors, self, and children).
+
+        Returns:
+            list: The family of the composite.
+        """
         return self.ancestors() + [self] + self.children(recursive=True)
 
     def setParent(self, parent):
+        """
+        Set the parent composite.
+
+        Args:
+            parent (Composite): The parent composite.
+        """
         self.__parent = None if parent is None else weakref.ref(parent)
 
     def children(self, recursive=False):
-        # Warning: this must satisfy the same condition as
-        # allItemsSorted() below.
+        """
+        Get the children of the composite.
 
+        Args:
+            recursive (bool, optional): If True, get all descendants recursively.
+
+        Returns:
+            list: The list of children.
+        """
         if recursive:
             result = self.__children[:]
             for child in self.__children:
@@ -80,6 +139,15 @@ class Composite(object):
             return self.__children
 
     def siblings(self, recursive=False):
+        """
+        Get the siblings of the composite.
+
+        Args:
+            recursive (bool, optional): If True, get all descendants of siblings recursively.
+
+        Returns:
+            list: The list of siblings.
+        """
         parent = self.parent()
         if parent:
             result = [child for child in parent.children() if child != self]
@@ -91,32 +159,67 @@ class Composite(object):
             return []
 
     def copy(self, *args, **kwargs):
+        """
+        Create a copy of the composite.
+
+        Returns:
+            Composite: The copied composite.
+        """
         kwargs["parent"] = self.parent()
         kwargs["children"] = [child.copy() for child in self.children()]
         return self.__class__(*args, **kwargs)
 
     def newChild(self, *args, **kwargs):
+        """
+        Create a new child composite.
+
+        Returns:
+            Composite: The new child composite.
+        """
         kwargs["parent"] = self
         return self.__class__(*args, **kwargs)
 
     def addChild(self, child):
+        """
+        Add a child composite.
+
+        Args:
+            child (Composite): The child composite to add.
+        """
         self.__children.append(child)
         child.setParent(self)
 
     def removeChild(self, child):
+        """
+        Remove a child composite.
+
+        Args:
+            child (Composite): The child composite to remove.
+        """
         self.__children.remove(child)
         # We don't reset the parent of the child, because that makes restoring
         # the parent-child relationship easier.
 
 
 class ObservableComposite(Composite):
+    """
+    A class representing an observable composite object in the composite pattern.
+    Inherits from Composite and adds observer pattern functionality.
+    """
+
     @observer.eventSource
     def __setstate__(self, state, event=None):  # pylint: disable=W0221
+        """
+        Set the state from unpickling with event notification.
+
+        Args:
+            state (dict): The state of the composite.
+            event (Event, optional): The event to notify.
+        """
         oldChildren = set(self.children())
         super(ObservableComposite, self).__setstate__(state)
         newChildren = set(self.children())
         childrenRemoved = oldChildren - newChildren
-        # pylint: disable=W0142
         if childrenRemoved:
             self.removeChildEvent(event, *childrenRemoved)
         childrenAdded = newChildren - oldChildren
@@ -125,32 +228,78 @@ class ObservableComposite(Composite):
 
     @observer.eventSource
     def addChild(self, child, event=None):  # pylint: disable=W0221
+        """
+        Add a child composite with event notification.
+
+        Args:
+            child (Composite): The child composite to add.
+            event (Event, optional): The event to notify.
+        """
         super(ObservableComposite, self).addChild(child)
         self.addChildEvent(event, child)
 
     def addChildEvent(self, event, *children):
+        """
+        Notify observers of the addition of children.
+
+        Args:
+            event (Event): The event to notify.
+            children (Composite): The children added.
+        """
         event.addSource(self, *children, **dict(type=self.addChildEventType()))
 
     @classmethod
     def addChildEventType(class_):
+        """
+        Get the event type for adding children.
+
+        Returns:
+            str: The event type.
+        """
         return "composite(%s).child.add" % class_
 
     @observer.eventSource
     def removeChild(self, child, event=None):  # pylint: disable=W0221
+        """
+        Remove a child composite with event notification.
+
+        Args:
+            child (Composite): The child composite to remove.
+            event (Event, optional): The event to notify.
+        """
         super(ObservableComposite, self).removeChild(child)
         self.removeChildEvent(event, child)
 
     def removeChildEvent(self, event, *children):
+        """
+        Notify observers of the removal of children.
+
+        Args:
+            event (Event): The event to notify.
+            children (Composite): The children removed.
+        """
         event.addSource(
             self, *children, **dict(type=self.removeChildEventType())
         )
 
     @classmethod
     def removeChildEventType(class_):
+        """
+        Get the event type for removing children.
+
+        Returns:
+            str: The event type.
+        """
         return "composite(%s).child.remove" % class_
 
     @classmethod
     def modificationEventTypes(class_):
+        """
+        Get the list of modification event types.
+
+        Returns:
+            list: The list of modification event types.
+        """
         try:
             eventTypes = super(
                 ObservableComposite, class_
@@ -164,15 +313,47 @@ class ObservableComposite(Composite):
 
 
 class CompositeCollection(object):
+    """
+    A collection of composite objects.
+
+    Methods:
+        append(composite): Add a composite to the collection.
+        extend(composites): Add multiple composites to the collection.
+        remove(composite): Remove a composite from the collection.
+        removeItems(composites): Remove multiple composites from the collection.
+        rootItems(): Get the root items of the collection.
+        allItemsSorted(): Get all items sorted by hierarchy.
+    """
+
     def __init__(self, initList=None, *args, **kwargs):
+        """
+        Initialize the collection with an optional list of initial composites.
+
+        Args:
+            initList (list, optional): Initial list of composites.
+        """
         super(CompositeCollection, self).__init__(*args, **kwargs)
         self.extend(initList or [])
 
     def append(self, composite, event=None):
+        """
+        Add a composite to the collection.
+
+        Args:
+            composite (Composite): The composite to add.
+            event (Event, optional): The event to notify.
+        """
         return self.extend([composite], event=event)
 
     @observer.eventSource
     def extend(self, composites, event=None):
+        """
+        Add multiple composites to the collection with event notification.
+
+        Args:
+            composites (list): The list of composites to add.
+            event (Event, optional): The event to notify.
+        """
         if not composites:
             return
         compositesAndAllChildren = self._compositesAndAllChildren(composites)
@@ -182,12 +363,28 @@ class CompositeCollection(object):
         self._addCompositesToParent(composites, event)
 
     def _compositesAndAllChildren(self, composites):
+        """
+        Get all composites and their children recursively.
+
+        Args:
+            composites (list): The list of composites.
+
+        Returns:
+            list: The list of composites and their children.
+        """
         compositesAndAllChildren = set(composites)
         for composite in composites:
             compositesAndAllChildren |= set(composite.children(recursive=True))
         return list(compositesAndAllChildren)
 
     def _addCompositesToParent(self, composites, event):
+        """
+        Add composites to their parent.
+
+        Args:
+            composites (list): The list of composites.
+            event (Event): The event to notify.
+        """
         for composite in composites:
             parent = composite.parent()
             if (
@@ -198,6 +395,13 @@ class CompositeCollection(object):
                 parent.addChild(composite, event=event)
 
     def remove(self, composite, event=None):
+        """
+        Remove a composite from the collection.
+
+        Args:
+            composite (Composite): The composite to remove.
+            event (Event, optional): The event to notify.
+        """
         return (
             self.removeItems([composite], event=event)
             if composite in self
@@ -206,6 +410,13 @@ class CompositeCollection(object):
 
     @observer.eventSource
     def removeItems(self, composites, event=None):
+        """
+        Remove multiple composites from the collection with event notification.
+
+        Args:
+            composites (list): The list of composites to remove.
+            event (Event, optional): The event to notify.
+        """
         if not composites:
             return
         compositesAndAllChildren = self._compositesAndAllChildren(composites)
@@ -215,12 +426,25 @@ class CompositeCollection(object):
         self._removeCompositesFromParent(composites, event)
 
     def _removeCompositesFromParent(self, composites, event):
+        """
+        Remove composites from their parent.
+
+        Args:
+            composites (list): The list of composites.
+            event (Event): The event to notify.
+        """
         for composite in composites:
             parent = composite.parent()
             if parent:
                 parent.removeChild(composite, event=event)
 
     def rootItems(self):
+        """
+        Get the root items of the collection.
+
+        Returns:
+            list: The list of root items.
+        """
         return [
             composite
             for composite in self
@@ -228,10 +452,12 @@ class CompositeCollection(object):
         ]
 
     def allItemsSorted(self):
-        """Returns a list of items and their children, so that if B is
-        a child, direct or not, of A, then A will come first in the
-        list."""
+        """
+        Get all items sorted by hierarchy.
 
+        Returns:
+            list: The list of all items sorted by hierarchy.
+        """
         result = []
         for item in self.rootItems():
             result.append(item)
@@ -240,8 +466,16 @@ class CompositeCollection(object):
 
 
 class CompositeSet(CompositeCollection, observer.ObservableSet):
+    """
+    A set of composite objects that is observable.
+    """
+
     pass
 
 
 class CompositeList(CompositeCollection, observer.ObservableList):
+    """
+    A list of composite objects that is observable.
+    """
+
     pass

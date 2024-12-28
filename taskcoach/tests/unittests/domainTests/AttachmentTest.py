57fef20741e490020c1daa16f3d0bdd16751437e
sylvain <szydlowskis@aol.com>
2024-12-28 11:30:26 +0100

    import test

--- taskcoach/tests/unittests/domainTests/AttachmentTest.py
+++ taskcoach/tests/unittests/domainTests/AttachmentTest.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
-import test
+from ... import test
 from taskcoachlib.domain import attachment
 from pubsub import pub
 

a61f00e2c30d909a12525dbfdbf5c1fdc3bb0c6d
sylvain <szydlowskis@aol.com>
2024-12-28 11:11:08 +0100

    change import

--- taskcoach/tools/dot.py
+++ taskcoach/tools/dot.py
@@ -16,7 +16,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os, re, glob, sys
+import glob
+import os
+import re
+import sys
 
 packages = []
 modules = []

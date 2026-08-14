ee0baf0c156f01397eb427cf85b1636f5d7b239a
sylvain <szydlowskis@aol.com>
2026-08-14 11:31:04 +0200

    Réorganise l'importation de lockfile dans test_lockfile.py

--- taskcoach/tests/unittests/thirdPartySoftwareTests/test_lockfile.py
+++ taskcoach/tests/unittests/thirdPartySoftwareTests/test_lockfile.py
@@ -18,7 +18,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # import lockfile
 import tempfile
-from taskcoachlib.thirdparty import lockfile
+
+# from taskcoachlib.thirdparty import lockfile
+import lockfile
 from ... import tctest
 
 

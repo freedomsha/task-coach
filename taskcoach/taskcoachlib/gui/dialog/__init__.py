c2ce8cfc5b6b3d4177f6ea045ff2c00938f93db0
sylvain <szydlowskis@aol.com>
2026-08-27 06:55:56 +0200

    Uncomment __all__ exports in __init__.py for improved module accessibility

--- taskcoach/taskcoachlib/gui/dialog/__init__.py
+++ taskcoach/taskcoachlib/gui/dialog/__init__.py
@@ -16,7 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-
 from taskcoachlib.gui.dialog import developer_message
 from taskcoachlib.gui.dialog import editor
 from taskcoachlib.gui.dialog import entry
